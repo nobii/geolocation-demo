44dd8d5d40383b67fb247659c6b043c4acd16dcb
fujisawa-shin <fujisawa-shin@kayac.com>
2016-09-28 19:32:50 +0900

    geolocationテスト

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,24 @@
-import Sample from './lib/Sample';
-import $ from 'jquery';
 
-const sample = new Sample({
-    name: 'world'
-});
 
-$('.wrapper').on('click', () => {
-    console.log(`hello, ${sample.name}.`);
-});
+(function () {
+    if (!navigator.geolocation) {
+        return;
+    }
+
+    const opts = {
+        enableHighAccuracy: true,
+        timeout: 8 * 1000,
+        maximumAge: 10 * 60 * 1000
+    };
+
+    navigator.geolocation.getCurrentPosition(
+        (pos) => {
+            alert(JSON.stringify(pos));
+        },
+        (err) => {
+            console.error(err);
+            alert('error!!');
+        },
+        opts
+    );
+})();
